c0fa1900e1a7dc6b3a77e05e3cdbe68d07f6ecac
Blaze <blaze@open.by>
2014-06-01 20:26:43 +0300

    OSD tool: fixes

--- tools/moc-osd.py
+++ tools/moc-osd.py
@@ -20,25 +20,27 @@ def cover_path(path):
     for entry in entries:
         if re.match('([^\s]+(?=\.(jpg|jpeg|png))\.)', entry):
             return path + '/' + entry
+        else:
+            return ''
 
 def main():
     output = lambda x : subprocess.getoutput(x)
+    filename = output('mocp -Q %file')
+    if not filename.find('/') == 0:
+        os._exit(0) #radio stream is playing
     artist = output('mocp -Q %artist')
     album = output('mocp -Q %album')
     tail = output("mocp -Q '%song (%tt)'")
-    filename = output('mocp -Q %file')
     path = filename.rsplit('/', 1)[0]
     year = find_year(path)
     cover = cover_path(path)
+    if not notify2.init('moc'):
+        os._exit(0) #notify2 is out of reach
 
-    if filename.find('/') == 0:
-        if not notify2.init('moc'):
-            os._exit(0) #notify2 is out of reach
-
-        ni = notify2.Notification('music on console', '%s <i>%s</i> %s<br /><b>%s</b>' % (artist, year, album, tail), cover)
-        ni.timeout = 5000
-        if not ni.show():
-            os._exit(1) #notify2 is not operational
+    ni = notify2.Notification('music on console', '%s <i>%s</i> %s<br /><b>%s</b>' % (artist, year, album, tail), cover)
+    ni.timeout = 5000
+    if not ni.show():
+        os._exit(1) #notify2 is not operational
 
 if __name__ == '__main__':
     main()
