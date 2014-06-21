ab48e264961232ad7a98aa422eedd1c029eb820d
Blaze <blaze@open.by>
2014-06-21 14:52:59 +0300

    LyricsDialog: code cleanup

--- src/lyricsdialog.cpp
+++ src/lyricsdialog.cpp
@@ -1,3 +1,22 @@
+/* ========================================================================
+*    Copyright (C) 2013-2014 Blaze <blaze@jabster.pl>
+*
+*    This file is part of eXo.
+*
+*    eXo is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    eXo is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with eXo.  If not, see <http://www.gnu.org/licenses/>.
+* ======================================================================== */
+
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
 #include <QUrl>
@@ -22,7 +41,7 @@ LyricsDialog::LyricsDialog(QWidget *parent) : QWidget(parent) {
 void LyricsDialog::showText(QNetworkReply *reply) {
     ui.stateLabel->setText("OK");
     if(reply->error() != QNetworkReply::NoError) {
-        ui.stateLabel->setText(tr("Error"));
+        ui.stateLabel->setText(tr("Network error"));
         ui.textBrowser->setText(reply->errorString());
         replyObject = 0;
         reply->deleteLater();
@@ -32,54 +51,44 @@ void LyricsDialog::showText(QNetworkReply *reply) {
     if(replyObject == reply) {
         replyObject = 0;
         reply->deleteLater();
-        QRegExp artistRgx("<artist>(.*)</artist>");
-        artistRgx.setMinimal(true);
-        QRegExp songRgx("<song>(.*)</song>");
+        QRegExp songRgx("<artist>(.*)</artist>.*<song>(.*)</song>.*"
+                        "<lyrics>(.*)</lyrics>.*<url>(.*)</url>");
         songRgx.setMinimal(true);
-        QRegExp lyricsRgx("<lyrics>(.*)</lyrics>");
-        lyricsRgx.setMinimal(true);
-        QRegExp urlRgx("<url>(.*)</url>");
-        urlRgx.setMinimal(true);
-        if(artistRgx.indexIn(content) < 0 || songRgx.indexIn(content) < 0 ||
-           lyricsRgx.indexIn(content) < 0 || urlRgx.indexIn(content) < 0) {
+        if(songRgx.indexIn(content) < 0) {
             ui.textBrowser->setHtml("<b>" + tr("Error") + "</b>");
             return;
         }
-        else if(lyricsRgx.cap(1) == "Not found") {
+        else if(songRgx.cap(3) == "Not found") {
             ui.textBrowser->setHtml("<b>" + tr("Not found") + "</b>");
             return;
         }
         else {
-            artistString = artistRgx.cap(1);
-            titleString = songRgx.cap(1);
+            artistString = songRgx.cap(1);
+            titleString = songRgx.cap(2);
         }
-        QString urlString = urlRgx.cap(1).toLatin1();
+        QString urlString = songRgx.cap(4).toLatin1();
         urlString.replace("http://lyrics.wikia.com/",
                      "http://lyrics.wikia.com/index.php?title=");
         urlString.append("&action=edit");
-        QUrl url = QUrl::fromEncoded(urlString.toLatin1());
-        QString referer = urlRgx.cap(1);
         QNetworkRequest request;
-        request.setUrl(url);
-        request.setRawHeader("Referer", referer.toLatin1());
-        ui.stateLabel->setText(tr("Receiving"));
+        request.setUrl(QUrl::fromEncoded(urlString.toLatin1()));
+        request.setRawHeader("Referer", songRgx.cap(4).toLatin1());
+        ui.stateLabel->setText(tr("Downloading"));
         httpObject->get(request);
         reply->deleteLater();
         return;
     }
-    content.replace("&lt;", "<");
-    QRegExp lyricsRgx("<lyrics>(.*)</lyrics>");
+    QRegExp lyricsRgx("&lt;lyrics>(.*)&lt;/lyrics>");
     lyricsRgx.indexIn(content);
     QString text = QString("<h2>%1 - %2</h2>").arg(artistString).arg(titleString);
     QString lyrics = lyricsRgx.cap(1);
     lyrics = lyrics.trimmed();
     lyrics.replace("\n", "<br>");
     if(lyrics.isEmpty())
-        ui.textBrowser->setHtml("<b>" + tr("Not found") + "</b>");
-    else {
+        text += "<b>" + tr("There's no lyrics for some reason.") + "</b>";
+    else
         text += lyrics;
-        ui.textBrowser->setHtml(text);
-    }
+    ui.textBrowser->setHtml(text);
     reply->deleteLater();
 }
 
@@ -101,13 +110,13 @@ void LyricsDialog::on_updatePushButton_clicked() {
 
 void LyricsDialog::on_prevButton_clicked() {
     PlayerInterface::instance()->prev();
-    ui.textBrowser->setHtml("");
+    ui.textBrowser->setHtml(tr("Please wait a second"));
     QTimer::singleShot(1500, this, SLOT(on_updatePushButton_clicked()));
 }
 
 void LyricsDialog::on_nextButton_clicked() {
     PlayerInterface::instance()->next();
-    ui.textBrowser->setHtml("");
+    ui.textBrowser->setHtml(tr("Please wait a second"));
     QTimer::singleShot(1500, this, SLOT(on_updatePushButton_clicked()));
 }
 
@@ -117,7 +126,7 @@ QString LyricsDialog::format(QString string) {
 }
 
 void LyricsDialog::search() {
-    ui.stateLabel->setText(tr("Receiving"));
+    ui.stateLabel->setText(tr("Searching"));
     setWindowTitle(QString(tr("%1 - %2")).arg(ui.artistLineEdit->text())
                    .arg(ui.titleLineEdit->text()));
     QNetworkRequest request;
