1812c63ea3a107a28d2ec0e2a8a5bbb60da29e40
Blaze <blaze@open.by>
2014-11-21 16:21:36 +0300

    PlayerInterface: using pure virtual functions

--- src/playerinterface.h
+++ src/playerinterface.h
@@ -53,22 +53,22 @@ signals:
     void updateStatus(QString, QString, QString, QString);
 
 public slots:
-    virtual void play() {}
-    virtual void pause() {}
-    virtual void prev() {}
-    virtual void next() {}
-    virtual void stop() {}
-    virtual void quit() {}
-    virtual void volu() {}
-    virtual void vold() {}
-    virtual void rewd() {}
-    virtual void frwd() {}
-    virtual void openWindow() {}
-    virtual void appendFile(QString) {}
+    virtual void play() = 0;
+    virtual void pause()= 0;
+    virtual void prev() = 0;
+    virtual void next() = 0;
+    virtual void stop() = 0;
+    virtual void quit() = 0;
+    virtual void volu() = 0;
+    virtual void vold() = 0;
+    virtual void rewd() = 0;
+    virtual void frwd() = 0;
+    virtual void openWindow() = 0;
+    virtual void appendFile(QString) = 0;
 
 protected slots:
-    virtual void update() {}
-    virtual void getInfo() {}
+    virtual void update() = 0;
+    virtual void getInfo() = 0;
 };
 
 #endif // PLAYERINTERFACE_H
