ceff38e8994e41076bea505e76860a9ed99b6201
blaze <blaze@vivaldi.net>
2016-12-10 17:12:19 +0300

    add BaseDialog class

---
+++ src/gui/basedialog.h
@@ -0,0 +1,34 @@
+/* ========================================================================
+*    Copyright (C) 2013-2016 Blaze <blaze@vivaldi.net>
+*
+*    This file is part of eXo.
+*
+*    eXo is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    eXo is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with eXo.  If not, see <http://www.gnu.org/licenses/>.
+* ======================================================================== */
+
+#ifndef BASEDIALOG_H
+#define BASEDIALOG_H
+
+#include <QWidget>
+
+class BaseDialog : public QWidget
+{
+    void keyPressEvent(QKeyEvent *e);
+
+public:
+    explicit BaseDialog(QWidget *parent = nullptr);
+
+};
+
+#endif // BASEDIALOG_H
