0693013548150e2976bacc61a9b6384a8a79dc42
blaze <blaze@vivaldi.net>
2016-12-12 15:08:55 +0300

    MOCInterface: remove debug header

--- src/core/mocinterface.cpp
+++ src/core/mocinterface.cpp
@@ -17,7 +17,6 @@
 *    along with eXo.  If not, see <http://www.gnu.org/licenses/>.
 * ======================================================================== */
 
-#include <QDebug>
 #include <QStringList>
 
 #include "mocinterface.h"
