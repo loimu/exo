46572360c218ae9cee6efc801e47c67e5f9b1f91
blaze <blaze@vivaldi.net>
2018-02-01 21:10:22 +0300

    RootObject: make all properties constant

--- src/dbus/rootobject.h
+++ src/dbus/rootobject.h
@@ -27,11 +27,11 @@ class RootObject : public QDBusAbstractAdaptor
 {
     Q_OBJECT
     Q_CLASSINFO("D-Bus Interface", "org.mpris.MediaPlayer2")
-    Q_PROPERTY(bool CanQuit READ canQuit)
-    Q_PROPERTY(bool CanRaise READ canRaise)
-    Q_PROPERTY(QString DesktopEntry READ desktopEntry)
-    Q_PROPERTY(bool HasTrackList READ hasTrackList)
-    Q_PROPERTY(QString Identity READ identity)
+    Q_PROPERTY(bool CanQuit READ canQuit CONSTANT)
+    Q_PROPERTY(bool CanRaise READ canRaise CONSTANT)
+    Q_PROPERTY(QString DesktopEntry READ desktopEntry CONSTANT)
+    Q_PROPERTY(bool HasTrackList READ hasTrackList CONSTANT)
+    Q_PROPERTY(QString Identity READ identity CONSTANT)
 
 public:
     explicit RootObject(QObject* parent = nullptr);
