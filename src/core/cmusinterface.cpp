48c76bd0904039c1790f4f23d75bd4a4c06338c8
blaze <blaze@vivaldi.net>
2018-02-14 09:03:49 +0300

    CmusInterface: port away from QRegExp, eliminate some defects

--- src/core/cmusinterface.cpp
+++ src/core/cmusinterface.cpp
@@ -20,6 +20,7 @@
 #include <QStringList>
 #include <QTime>
 #include <QProcess>
+#include <QRegularExpression>
 
 #include "cmusinterface.h"
 
@@ -57,18 +58,17 @@ void CmusInterface::runPlayer() {
                                               QStringLiteral("cmus")});
 }
 
-QString CmusInterface::find(const QString& string, const QString& regEx) {
-    QRegExp findRgx(regEx);
-    findRgx.setMinimal(true);
-    findRgx.indexIn(string);
-    return findRgx.cap(1);
+QString CmusInterface::find(const QString& string, const QString& regexp) {
+    QRegularExpression re(regexp);
+    QRegularExpressionMatch match = re.match(string);
+    return match.hasMatch() ? match.captured(1) : QString();
 }
 
 PIState CmusInterface::updateInfo() {
     QString info = cmus->readAllStandardOutput();
     if(info.isEmpty())
         return PIState::Offline;
-    QString string = find(info, QStringLiteral("status\\s(.*)\\n"));
+    QString string = find(info, QStringLiteral("status\\s(.*)"));
     if(string == QLatin1String("stopped"))
         return PIState::Stop;
     PIState state = PIState::Offline;
@@ -76,31 +76,30 @@ PIState CmusInterface::updateInfo() {
         state = PIState::Play;
     if(string == QLatin1String("paused"))
         state = PIState::Pause;
-    track.artist = find(info, QStringLiteral("tag\\sartist\\s(.*)\\n"));
-    track.title = find(info, QStringLiteral("tag\\stitle\\s(.*)\\n"));
-    track.album = find(info, QStringLiteral("tag\\salbum\\s(.*)\\n"));
-    track.file = find(info, QStringLiteral("file\\s(.*)\\n"));
-    track.totalSec = find(info, QStringLiteral("duration\\s(.*)\\n")).toInt();
-    track.currSec = find(info, QStringLiteral("position\\s(.*)\\n")).toInt();
-    track.totalTime = QTime().addSecs(
-                track.totalSec).toString(QStringLiteral("mm:ss"));
+    track.artist = find(info, QStringLiteral("tag\\sartist\\s(.*)"));
+    track.title = find(info, QStringLiteral("tag\\stitle\\s(.*)"));
+    track.album = find(info, QStringLiteral("tag\\salbum\\s(.*)"));
+    track.file = find(info, QStringLiteral("file\\s(.*)"));
+    track.totalSec = find(info, QStringLiteral("duration\\s(.*)")).toInt();
+    track.currSec = find(info, QStringLiteral("position\\s(.*)")).toInt();
+    track.totalTime = QTime(0, 0)
+            .addSecs(track.totalSec).toString(QStringLiteral("m:ss"));
     track.caption = track.artist.isEmpty()
             ? track.title
-            : track.artist + QLatin1String(" - ") + track.title;
+            : QString(QStringLiteral("%1 - %2 (%3)")).arg(
+                  track.artist, track.title, track.album);
     track.isStream = !track.file.startsWith(QChar::fromLatin1('/'));
     if(!track.isStream)
         return state;
-    QString title = find(info, QStringLiteral("stream\\s(.*)\\n"));
+    QString title = find(info, QStringLiteral("stream\\s(.*)"));
     track.caption += QLatin1String("<br />") + title;
     track.totalSec = 8*60;
     if(!title.isEmpty()) {
-        QRegExp artistRgx(QStringLiteral("^(.*)\\s-\\s"));
-        artistRgx.setMinimal(true);
-        artistRgx.indexIn(title);
-        track.artist = artistRgx.cap(1);
-        QRegExp titleRgx(QStringLiteral("\\s-\\s(.*)$"));
-        titleRgx.indexIn(title);
-        track.title = titleRgx.cap(1);
+        QString dash = QStringLiteral(" - ");
+        if(title.contains(dash)) {
+            track.artist = title.section(dash, 0, 0);
+            track.title = title.section(dash, 1, -1);
+        }
     }
     return state;
 }
@@ -156,8 +155,11 @@ void CmusInterface::showPlayer() {
 }
 
 bool CmusInterface::openUri(const QString& file) {
-    return QProcess::startDetached(
-                cli, QStringList() << QStringLiteral("-q") << file);
+    QProcess::startDetached(cli, QStringList() << QStringLiteral("-q")
+                            << QStringLiteral("-c")); // clear queue
+    QProcess::startDetached(cli, QStringList() << QStringLiteral("-q")
+                            << file); // append file to queue
+    return QProcess::startDetached(cli, QStringList() << QStringLiteral("-n"));
 }
 
 bool CmusInterface::appendFile(const QStringList& files) {
@@ -167,5 +169,6 @@ bool CmusInterface::appendFile(const QStringList& files) {
 
 void CmusInterface::timerEvent(QTimerEvent *event) {
     Q_UNUSED(event);
+    cmus->close();
     cmus->start(cli, QStringList{QStringLiteral("-Q")});
 }
