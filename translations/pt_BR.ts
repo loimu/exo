b64fcc01542888e221de5d21254a6f24d375db50
marcelocripe <69278712+marcelocripe@users.noreply.github.com>
2023-03-28 10:32:41 -0300

    Add Brazilian Portuguese translation file

---
+++ translations/pt_BR.ts
@@ -0,0 +1,311 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR" sourcelanguage="en">
+<!-- Translation by  marcelocripe 03-19-2023 -->
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../src/gui/aboutdialog.cpp" line="33"/>
+        <source>About eXo</source>
+        <translation>Sobre o eXo</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/aboutdialog.cpp" line="51"/>
+        <source>About &amp;Qt</source>
+        <translation>Sobre o &amp;Qt</translation>
+    </message>
+</context>
+<context>
+    <name>BookmarkManager</name>
+    <message>
+        <location filename="../src/gui/bookmarkmanager.cpp" line="39"/>
+        <source>Bookmark Manager</source>
+        <translation>Gerenciador de Favoritos</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/bookmarkmanager.cpp" line="47"/>
+        <source>&amp;Delete</source>
+        <translation>&amp;Excluir</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/bookmarkmanager.cpp" line="48"/>
+        <source>Delete selected item</source>
+        <translation>Excluir o item selecionado</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/bookmarkmanager.cpp" line="53"/>
+        <source>Rename selected item</source>
+        <translation>Renomear o item selecionado</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/bookmarkmanager.cpp" line="62"/>
+        <source>Copy URL to clipboard</source>
+        <translation>Copiar o endereço de URL para a área de transferência</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/bookmarkmanager.cpp" line="65"/>
+        <source>Move up</source>
+        <translation>Mover para cima</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/bookmarkmanager.cpp" line="68"/>
+        <source>Move down</source>
+        <translation>Mover para baixo</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/bookmarkmanager.cpp" line="95"/>
+        <location filename="../src/gui/bookmarkmanager.cpp" line="175"/>
+        <source>Name: </source>
+        <translation>Nome: </translation>
+    </message>
+</context>
+<context>
+    <name>ConsoleAuth</name>
+    <message>
+        <location filename="../src/core/consoleauth.cpp" line="39"/>
+        <source>Last.fm authentication</source>
+        <translation>Autenticação do Last.fm</translation>
+    </message>
+    <message>
+        <location filename="../src/core/consoleauth.cpp" line="40"/>
+        <source>Login:</source>
+        <translation>Nome de Usuário:</translation>
+    </message>
+    <message>
+        <location filename="../src/core/consoleauth.cpp" line="43"/>
+        <source>Password:</source>
+        <translation>Senha:</translation>
+    </message>
+    <message>
+        <location filename="../src/core/consoleauth.cpp" line="52"/>
+        <source>Try again? (y/n)</source>
+        <translation>Você quer tentar novamente? (s/n)</translation>
+    </message>
+    <message>
+        <location filename="../src/core/consoleauth.cpp" line="57"/>
+        <source>y</source>
+        <translatorcomment>the first letter of &quot;yes&quot;</translatorcomment>
+        <translation>A primeira letra da opção &quot;sim&quot;</translation>
+    </message>
+    <message>
+        <location filename="../src/core/consoleauth.cpp" line="58"/>
+        <source>Exiting authentication procedure</source>
+        <translation>Sair do procedimento de autenticação</translation>
+    </message>
+    <message>
+        <location filename="../src/core/consoleauth.cpp" line="68"/>
+        <source>Authentication succesful!</source>
+        <translation>A autenticação foi bem-sucedida!</translation>
+    </message>
+</context>
+<context>
+    <name>LyricsDialog</name>
+    <message>
+        <location filename="../src/gui/lyricsdialog.cpp" line="64"/>
+        <source>Lyrics</source>
+        <translation>Letra</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/lyricsdialog.cpp" line="70"/>
+        <source>artist</source>
+        <translation>Artista</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/lyricsdialog.cpp" line="71"/>
+        <source>Edit artist and then press Enter</source>
+        <translation>Edite o nome do artista e pressione a tecla &quot;Enter&quot;</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/lyricsdialog.cpp" line="75"/>
+        <source>title</source>
+        <translation>Título</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/lyricsdialog.cpp" line="76"/>
+        <source>Edit title and then press Enter</source>
+        <translation>>Edite o título da faixa e pressione a tecla &quot;Enter&quot;</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/lyricsdialog.cpp" line="89"/>
+        <source>Auto</source>
+        <translation>Automático</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/lyricsdialog.cpp" line="90"/>
+        <source>Autoupdate track when changed</source>
+        <translation>Atualização automática da faixa quando for modificada</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/lyricsdialog.cpp" line="94"/>
+        <source>Update</source>
+        <translation>Atualizar</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/lyricsdialog.cpp" line="95"/>
+        <source>Get lyrics for the current track</source>
+        <translation>Obtenha a letra da faixa atual</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/lyricsdialog.cpp" line="130"/>
+        <source>Network error</source>
+        <translation>Ocorreu um erro de rede</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/lyricsdialog.cpp" line="155"/>
+        <source>Preparing</source>
+        <translation>Está sendo preparado</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/lyricsdialog.cpp" line="166"/>
+        <source>No lyrics available</source>
+        <translation>Nenhuma letra está disponível</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/lyricsdialog.cpp" line="200"/>
+        <source>Fetching</source>
+        <translation>Buscando</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/lyricsdialog.cpp" line="146"/>
+        <source>Not found</source>
+        <translation>Não foi encontrado</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="94"/>
+        <source>Application is already running</source>
+        <translation>O aplicativo já está em execução</translation>
+    </message>
+</context>
+<context>
+    <name>ScrobblerAuth</name>
+    <message>
+        <location filename="../src/lastfm/scrobblerauth.cpp" line="45"/>
+        <source>network error, try again</source>
+        <translation>Ocorreu um erro de rede. Por favor, tente novamente</translation>
+    </message>
+    <message>
+        <location filename="../src/lastfm/scrobblerauth.cpp" line="59"/>
+        <source>wrong data or connection error, try again</source>
+        <translation>Os dados não estão corretos ou ocorreu um erro de rede. Por favor, tente novamente</translation>
+    </message>
+</context>
+<context>
+    <name>ScrobblerSettings</name>
+    <message>
+        <location filename="../src/gui/scrobblersettings.cpp" line="32"/>
+        <source>Scrobbler Settings</source>
+        <translation>Configurações do Scrobbler</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/scrobblersettings.cpp" line="36"/>
+        <source>login</source>
+        <translation>Nome de Usuário</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/scrobblersettings.cpp" line="40"/>
+        <source>password</source>
+        <translation>Senha</translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="../src/gui/trayicon.cpp" line="62"/>
+        <source>Player</source>
+        <translation>Reprodutor</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/trayicon.cpp" line="65"/>
+        <source>A&amp;dd ...</source>
+        <translation>A&amp;dicionar...</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/trayicon.cpp" line="67"/>
+        <source>&amp;Lyrics</source>
+        <translation>&amp;Letra</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/trayicon.cpp" line="70"/>
+        <source>&amp;Play</source>
+        <translation>&amp;Reproduzir</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/trayicon.cpp" line="73"/>
+        <source>P&amp;ause</source>
+        <translation>Pa&amp;usar</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/trayicon.cpp" line="77"/>
+        <source>P&amp;rev</source>
+        <translation>&amp;Anterior</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/trayicon.cpp" line="80"/>
+        <source>&amp;Next</source>
+        <translation>Pró&amp;ximo</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/trayicon.cpp" line="83"/>
+        <source>&amp;Stop</source>
+        <translation>&amp;Parar</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/trayicon.cpp" line="86"/>
+        <source>A&amp;bout</source>
+        <translation>&amp;Sobre</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/trayicon.cpp" line="88"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Sair</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/trayicon.cpp" line="91"/>
+        <source>Bookmark &amp;Current</source>
+        <translation>&amp;Favorito Atual</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/trayicon.cpp" line="96"/>
+        <source>Bookmark &amp;Manager</source>
+        <translation>&amp;Gerenciador de Favoritos</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/trayicon.cpp" line="101"/>
+        <source>&amp;Close player on exit</source>
+        <translation>Fec&amp;he o reprodutor ao sair do programa</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/trayicon.cpp" line="110"/>
+        <source>&amp;Enable scrobbling</source>
+        <translation>&amp;Ativar o scroblling</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/trayicon.cpp" line="133"/>
+        <source>&amp;Edit with</source>
+        <translation>&amp;Editar com</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/trayicon.cpp" line="157"/>
+        <source>Lin&amp;ks</source>
+        <translation>&amp;Vínculos</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/trayicon.cpp" line="163"/>
+        <source>Se&amp;ttings</source>
+        <translation>&amp;Configurações</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/trayicon.cpp" line="223"/>
+        <source>Player isn&apos;t running</source>
+        <translation>O reprodutor não estão em execução</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/trayicon.cpp" line="224"/>
+        <source>Stopped</source>
+        <translation>Parado</translation>
+    </message>
+</context>
+</TS>
